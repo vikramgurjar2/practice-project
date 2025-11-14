3710ef39ae7f54edca11d6bb557edc404203d965
vikram gurjar <vikramgurjar9179@gmail.com>
2025-11-14 16:52:45 +0530

    graceful shutting down

--- cmd/practice-project/main.go
+++ cmd/practice-project/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/vikramgurjar2/practice-project/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello, Practice Project!")
+	//load config
+	cfg := config.MustLoad()
+
+	//database setup
+	//setup routes we will use inbuilt router mathods for routing and path
+	router := http.NewServeMux()
+	router.HandleFunc("GET /api/students", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome to the students api"))
+	})
+
+	//setup server
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+	//start server
+	fmt.Println("server started at", cfg.Addr)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("error starting server:", err)
+	}
+
+	fmt.Println("server started at", cfg.Addr)
+
 }
---
+++ internal/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HTTPServer struct {
+	Addr string `yaml:"address"`
+}
+
+type Config struct {
+	Env         string `yaml:"env" env:"ENV" env-required:"true" env-default:"production"`
+	StoragePath string `yaml:"storage_path"  env:"STORAGE_PATH" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+func MustLoad() *Config {
+	var configPath string
+	configPath = os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		flags := flag.String("config", "", "path to the configuration file")
+		flag.Parse()
+		configPath = *flags
+
+		if configPath == "" {
+			{
+				log.Fatal("configuration path must be provided via CONFIG_PATH env variable or --config flag")
+			}
+		}
+	}
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist:%s", configPath)
+	}
+
+	var cfg Config
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatalf("failed to read config:%v", err)
+	}
+
+	return &cfg
+
+}
